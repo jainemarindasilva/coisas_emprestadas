add59da080ee88f136b36e7d9c20997248a9e797
Jaíne <jainemarins@gmail.com>
2022-03-08 15:03:52 -0300

    [US001] Cria CRUD Contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,4 +6,17 @@ class ContactsController < ApplicationController
   def create
 
   end
+
+  def edit
+    id = params[:id]
+    @contact = Contact.find(id)
+    #byebug
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    @contact.update(params[:contact])
+    byebug
+  end
+
 end
