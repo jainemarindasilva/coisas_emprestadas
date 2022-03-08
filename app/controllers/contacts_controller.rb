6becdbca5563cd1de08aaee3b47e3249350164b6
Jaíne <jainemarins@gmail.com>
2022-03-08 14:57:07 -0300

    Revert "[US001] Cria CRUD contacts"
    This reverts commit 4f0bffc3e14af38e93d122b4e0821e2323cad65d.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,17 +6,4 @@ class ContactsController < ApplicationController
   def create
 
   end
-
-  def edit
-    id = params[:id]
-    @contact = Contact.find(id)
-    #byebug
-  end
-
-  def update
-    @contact = Contact.find(params[:id])
-    @contact.update(params[:contact])
-    byebug
-  end
-
 end
